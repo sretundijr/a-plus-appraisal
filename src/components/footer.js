fa50c767f95750452e82cd59c90d719ab3f7b833
sretundijr <sretundijr@gmail.com>
2018-02-24 17:27:58 -0700

    footer added

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const footer = () => {
+  return (
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>Hello footer</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default footer;
